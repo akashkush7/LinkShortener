c8ff1e8c4dd4c023e0a58ca9e02af9879d966761
akashkush7 <akashkushwaha060@gmail.com>
2024-08-18 10:04:42 +0530

    update to the copyright and copy text functionality

--- src/app/Components/Footer.js
+++ src/app/Components/Footer.js
@@ -1,7 +1,10 @@
 
 const Footer = () => {
+
+    const date = new Date().getFullYear();
+
     return (
-        <footer className="text-center w-100 bg-dark text-white">Copyright</footer>
+        <footer className="text-center w-100 bg-dark text-white">© {date}. All Rights Reserved.</footer>
     )
 }
 
